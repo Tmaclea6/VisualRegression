bca0a45467a0e77f139094361e22d6af3888ee9b
Tyler MacLean <60271478+Tmaclea6@users.noreply.github.com>
2023-10-10 13:30:08 -0300

    Update screenshot compare.py
    Added full page screenshot and diff overlay

--- screenshot compare.py
+++ screenshot compare.py
@@ -8,11 +8,13 @@
 
 import pyautogui
 from PIL import Image
+from io import BytesIO
 import numpy as np
 import PySimpleGUI as sg
 from pathlib import Path
 from selenium import webdriver
 import os
+import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -39,9 +41,73 @@ def save_screenshot(url, driver: webdriver.Chrome, path: str = '/tmp/screenshot.
     required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
     required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
     driver.set_window_size(required_width, required_height)
-    # driver.save_screenshot(path)  # has scrollbar
-    driver.find_element("name", 'body').screenshot(path)  # avoids scrollbar
+    driver.save_screenshot(path)  # has scrollbar
+    # driver.find_element("name", 'body').screenshot(path)  # avoids scrollbar
     driver.set_window_size(original_size['width'], original_size['height'])
+def ss(url, driver: webdriver.Chrome, path: str = '/tmp/screenshot.png'):
+    print("Starting chrome full page screenshot workaround ...")
+    driver.get(url)
+    file = path
+    total_width = driver.execute_script("return document.body.offsetWidth")
+    total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
+    viewport_width = driver.execute_script("return document.body.clientWidth")
+    viewport_height = driver.execute_script("return window.innerHeight")
+    print("Total: ({0}, {1}), Viewport: ({2},{3})".format(total_width, total_height, viewport_width, viewport_height))
+    rectangles = []
+
+    i = 0
+    while i < total_height:
+        ii = 0
+        top_height = i + viewport_height
+
+        if top_height > total_height:
+            top_height = total_height
+
+        while ii < total_width:
+            top_width = ii + viewport_width
+
+            if top_width > total_width:
+                top_width = total_width
+
+            print("Appending rectangle ({0},{1},{2},{3})".format(ii, i, top_width, top_height))
+            rectangles.append((ii, i, top_width, top_height))
+
+            ii = ii + viewport_width
+
+        i = i + viewport_height
+
+    stitched_image = Image.new('RGB', (total_width, total_height))
+    previous = None
+    part = 0
+
+    for rectangle in rectangles:
+        if not previous is None:
+            driver.execute_script("window.scrollTo({0}, {1})".format(rectangle[0], rectangle[1]))
+            print("Scrolled To ({0},{1})".format(rectangle[0], rectangle[1]))
+            time.sleep(0.2)
+
+        file_name = "part_{0}.png".format(part)
+        print("Capturing {0} ...".format(file_name))
+
+        driver.get_screenshot_as_file(file_name)
+        screenshot = Image.open(file_name)
+
+        if rectangle[1] + viewport_height > total_height:
+            offset = (rectangle[0], total_height - viewport_height)
+        else:
+            offset = (rectangle[0], rectangle[1])
+
+        print("Adding to stitched image with offset ({0}, {1})".format(offset[0], offset[1]))
+        stitched_image.paste(screenshot, offset)
+
+        del screenshot
+        os.remove(file_name)
+        part = part + 1
+        previous = rectangle
+
+    stitched_image.save(file)
+    print("Finishing chrome full page screenshot workaround...")
+    return True
 
 
 def compare_images(image1_path, image2_path, output_path):
@@ -77,7 +143,8 @@ def compare_images(image1_path, image2_path, output_path):
 
     # Save the resulting image
     result_image = Image.fromarray(red_image)
-    result_image.save(output_path)
+    new_img = Image.blend(image1, result_image, 0.5)
+    new_img.save(output_path)
 
     return percentage_difference
 
@@ -110,7 +177,7 @@ while True:
                         lines.append(cleaned_line)
 
                     for i in lines:
-                        save_screenshot(i, driver=driver, path=(b+"image " + str(n) + ".png"))
+                        ss(i, driver=driver, path=(b+"image " + str(n) + ".png"))
                         n = n + 1
 
                 break
@@ -138,7 +205,7 @@ while True:
                         lines.append(cleaned_line)
 
                     for i in lines:
-                        save_screenshot(i, driver=driver, path=(a+"image " + str(n) + ".png"))
+                        ss(i, driver=driver, path=(a+"image " + str(n) + ".png"))
                         n = n + 1
 
                 break
